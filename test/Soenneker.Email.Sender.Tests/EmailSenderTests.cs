e570a0ab031456595e2fc0ff8b42600388dc702d
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:42 -0500

    TUnit conversion

--- test/Soenneker.Email.Sender.Tests/EmailSenderTests.cs
+++ test/Soenneker.Email.Sender.Tests/EmailSenderTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Email.Sender.Tests;
 
-[Collection("Collection")]
-public class EmailSenderTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class EmailSenderTests : HostedUnitTest
 {
-    public EmailSenderTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public EmailSenderTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
